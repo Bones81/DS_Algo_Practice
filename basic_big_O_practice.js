fc284e782f87dac5b744ca035e760e18e2dbc059
Nathan Freeman <NathanLFreeman@gmail.com>
2022-07-22 17:58:14 -0400

    sets practice

--- basic_big_O_practice.js
+++ basic_big_O_practice.js
@@ -1,38 +1,58 @@
-// this example has linear O(n) time complexity
-const sumUp = (n) => {
-  let result = 0;
-  for (let i = 0; i <= n; i++) {
-    result += i
-  }
-  return result
+// // this example has linear O(n) time complexity
+// const sumUp = (n) => {
+//   let result = 0;
+//   for (let i = 0; i <= n; i++) {
+//     result += i
+//   }
+//   return result
+// }
+
+// console.log(sumUp(4))
+
+// //same function with O(1) constant time
+// const constantSumUp = (n) => {
+//   return (n/2) * (1 + n)
+// }
+
+// console.log(constantSumUp(4))
+
+
+// // sum an array, define time complexity, find lowest possible time complexity
+
+// const sumArray = (array) => {
+//   let result = 0
+//   for (let num of array) {
+//     result += num
+//   }
+//   return result
+//   //should be O(n)
+// }
+
+// console.log(sumArray([1,3,4,6,10]))
+
+// const reduceArray = (array) => {
+//   return array.reduce((sum, num) => sum + num, 0)
+//   //should also be O(n), even though it's fewer lines of code
+// }
+
+// console.log(reduceArray([1,3,4,6,10]))
+
+
+//Sets
+const ids = new Set()
+
+ids.add('abc')
+ids.add(1)
+ids.add('bb2')
+ids.add(1)
+
+for (const el of ids) {
+  console.log(el);
 }
 
-console.log(sumUp(4))
+// console.log(ids[1]); doesn't work like an array. Instead use:
+console.log(ids.has('abc'));
 
-//same function with O(1) constant time
-const constantSumUp = (n) => {
-  return (n/2) * (1 + n)
-}
+ids.delete('bb2')
 
-console.log(constantSumUp(4))
-
-
-// sum an array, define time complexity, find lowest possible time complexity
-
-const sumArray = (array) => {
-  let result = 0
-  for (let num of array) {
-    result += num
-  }
-  return result
-  //should be O(n)
-}
-
-console.log(sumArray([1,3,4,6,10]))
-
-const reduceArray = (array) => {
-  return array.reduce((sum, num) => sum + num, 0)
-  //should also be O(n), even though it's fewer lines of code
-}
-
-console.log(reduceArray([1,3,4,6,10]))
+console.log(ids);
